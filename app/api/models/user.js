9bcffa6f942cfa2710250a98683e9111d2341501
odholden <odholden@gmail.com>
2015-12-14 11:25:21 +0000

    added passport local and bcrypt packages for encryption

---
+++ app/api/models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var bcrypt   = require("bcrypt-nodejs");
+
+var userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  image:    { type: String },
+  email:    { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  groups: [ { type: mongoose.Schema.ObjectId, ref: 'Group'} ]
+})
+
+userSchema.statics.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
