d5b0c399d1226a4e8714b07cd24b4bc85da04c0a
odholden <odholden@gmail.com>
2015-12-14 10:03:16 +0000

    added bower to project

--- socket-setup/app.js
+++ socket-setup/app.js
@@ -1,10 +1,19 @@
-var app  = require("express")();
-var http = require('http').Server(app);
+var app      = require('express')();
+var http     = require('http').Server(app);
+var io       = require('socket.io')(http);
+var path     = require("path");
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, "index.html")) ;
 });
 
-http.listen(3000, function() {
-  console.log("listening on 3000")
-})
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on 3000');
+});
